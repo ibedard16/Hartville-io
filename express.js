0f34382aba6fd2aac27f8edf6d53fb55c69e77aa
Isaac Bedard <ibedard@biomedscienceacademy.org>
2015-07-23 14:33:38 -0400

    Created server that uses response.sendFile to load the webpage.

---
+++ express.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var fs = require('fs');
+var express = require('express');
+var url = require('url');
+var app = express();
+
+app.set("view engine", "ejs");
+
+function anyHandler(request, response) {
+	var myUrl = url.parse(request.url).pathname//.substring(1);
+	
+	if(myUrl === "") {
+		response.redirect("views/index.html");
+		return;
+	};
+	
+	//fs.createReadStream(myUrl)
+	response.sendFile(__dirname + myUrl)
+		/*.on('error', function(err) {
+			response.writeHead(500);
+			response.end('Error encountered (the file probably doesn\'t exist.) File accessed: ' + myUrl);
+		})*/
+	//.pipe(response);
+};
+
+app.get("*", anyHandler);
+
+function listener(){
+	console.log('app is listening on http://localhost:8000');
+};
+
+app.listen(8000, listener);
