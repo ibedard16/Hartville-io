39a8812de088ecb143bb4742f6d5713685cbe637
Isaac Bedard <ibedard@biomedscienceacademy.org>
2015-07-29 16:43:23 -0400

    Changed the base link to get links to work. Links to be fully repaired later.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,1 @@
-var app = angular.module("Hartville_io", [])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'blogDisplay.html',
-                controller: 'BlogController'
-            })
-            .when('/phones/:phoneId', {
-                templateUrl: 'partials/phone-detail.html',
-                controller: 'PhoneDetailCtrl'
-            })
-           
-  }]);
+var app = angular.module("Hartville_io", []);  
