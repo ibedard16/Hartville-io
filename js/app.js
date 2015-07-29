23c8f18e3a6f1866b05047fd235ae7c275878ed0
Isaac Bedard <ibedard@biomedscienceacademy.org>
2015-07-29 16:22:21 -0400

    Let's hope this works.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-var app = angular.module("Hartville_io", []);
+var app = angular.module("Hartville_io", [])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'blogDisplay.html',
+                controller: 'BlogController'
+            })
+            .when('/phones/:phoneId', {
+                templateUrl: 'partials/phone-detail.html',
+                controller: 'PhoneDetailCtrl'
+            })
+           
+  }]);
