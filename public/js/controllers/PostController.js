d9d9abaf6fce640e49ce83a52fe1e3313217ca90
Isaac Bedard <ibedard@biomedscienceacademy.org>
2015-08-30 02:31:53 +0000

    removed annoying console.log statements that were no longer nescessary

--- public/js/controllers/PostController.js
+++ public/js/controllers/PostController.js
@@ -1,9 +1,7 @@
 app.controller('PostController', ['$scope', '$routeParams', '$filter', 'posts', 'authors', 'events', function($scope, $routeParams, $filter, posts, authors, events){
 	posts.get({post:$routeParams.id}).then(function(data) {
-		console.log(JSON.stringify(data));
 		if (data.status == 404) {
 			$scope.error = true;
-			console.log($scope.error);
 		} else {
 			$scope.post = data.post;
 		}
