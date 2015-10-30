792bf12610aa99fbf63fd413a22076fefe47dbee
Isaac Bedard <ibedard@biomedscienceacademy.org>
2015-10-30 12:08:28 +0000

    Make sidebar display author bio when viewing post.

--- public/js/directives/navigation/websiteSidebar.js
+++ public/js/directives/navigation/websiteSidebar.js
@@ -1,10 +1,23 @@
-app.directive('websiteSidebar', ['$routeParams', function ($routeParams) {
+app.directive('websiteSidebar', ['$routeParams', 'User', 'Post', function ($routeParams, User, Post) {
 	return {
 		restrict: 		'E',
 		templateUrl:	'js/directives/navigation/partials/website-sidebar.html',
 		scope: {
-        	author: '=author',
+        	authorId: '=authorId',
         	posts: '=posts'
     	},
+    	link: function (scope, element) {
+    		scope.$watch('authorId', function () {
+    			if (!!scope.authorId) {
+		    		User.get({id:scope.authorId}, function (data) {
+		    			scope.author = data;
+		    		});
+    			}
+    			Post.get({authorId:scope.authorId, limitTo:5}, function (data) {
+	    			scope.posts = data.posts;
+	    		});
+    		});
+    	}
 	};
+	
 }]);
